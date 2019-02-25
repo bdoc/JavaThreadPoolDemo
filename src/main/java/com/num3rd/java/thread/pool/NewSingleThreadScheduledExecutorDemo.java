0a04f0dd56753e6f504f6c55336222a565336be5
Tobyhong <tobyhong@lagou.com>
2019-02-25 13:05:51 +0800

    newSingleThreadScheduledExecutor

---
+++ src/main/java/com/num3rd/java/thread/pool/NewSingleThreadScheduledExecutorDemo.java
@@ -0,0 +1,22 @@
+package com.num3rd.java.thread.pool;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class NewSingleThreadScheduledExecutorDemo {
+    public static void main(String[] args) {
+        ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
+        scheduledExecutorService.schedule(new Runnable() {
+            public void run() {
+                System.out.println("delay 3 seconds");
+            }
+        }, 3, TimeUnit.SECONDS);
+
+        scheduledExecutorService.scheduleAtFixedRate(new Runnable() {
+            public void run() {
+                System.out.println("delay 1 seconds, and execute every 3 seconds");
+            }
+        }, 1, 3, TimeUnit.SECONDS);
+    }
+}
